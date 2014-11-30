516378f374cdf3b4ce933c999caa223a3ab813a3
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-30 10:47:26 +0100

    added some controls in quadratic_equatiion.ml

--- ocaml/quadratic_equation.ml
+++ ocaml/quadratic_equation.ml
@@ -6,15 +6,19 @@ let square x = x *. x;;
 let delta  a b c = ( square b-. (4. *. a *. c ));;
 
 let   solve a b c   =
-	if   delta a b c  >= 0. then
-		let   x1=( -.b +. sqrt(delta a b c )) /. (2. *. a)
-			and x2=  (-.b -.  sqrt(delta a b c ))  /. (2. *. a)  
-		in 
-			Printf.printf "x1 =%.5f x2=%.5f "  x1  x2
-	else 
-		let   x= (-.b   /. (2. *. a))
-			and i=     sqrt(4. *. a *. c -. square b)  /. (2. *. a) 
-		in Printf.printf "x+ =%.5f + i%.5f  x-=%.5f - i%.5f "  x  i  x i
+	if a=0. 
+		then Printf.printf "Not a quadratic equation\n"
+		else
+			if   delta a b c  >= 0.
+				then
+					let   x1=( -.b +. sqrt(delta a b c )) /. (2. *. a)
+						and x2=  (-.b -.  sqrt(delta a b c ))  /. (2. *. a)  
+					in 
+						Printf.printf "x1 =%.5f x2=%.5f \n"  x1  x2
+				else 
+					let   x= (-.b   /. (2. *. a))
+						and i=     sqrt(4. *. a *. c -. square b)  /. (2. *. a) 
+					in Printf.printf "x+ =%.5f+i%.5f  x-=%.5f-i%.5f \n"  x  i  x i
 	;;
 
 
