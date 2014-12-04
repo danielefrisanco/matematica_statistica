64c384bb4177228e1000374aa2e6bc379e7bf417
danielefrisanco <daniele.frisanco@gmail.com>
2014-12-04 18:07:46 +0100

    distanza ocaml

---
+++ ocaml/distanza_fra_due_punti.ml
@@ -0,0 +1,25 @@
+(* ocaml distanza_fra_due_punti.ml  ~-.3.05 5.1 2.85 ~-.8.6*)
+  
+
+
+ 
+
+let   distanza x1 y1 x2 y2  =
+	
+	
+				(*	 sqrt(( (x1 -. x2 )** 2.)+. ((y1 -. y2)**2.))*)
+	Printf.printf   "dist %.5f   \n" (sqrt(((x1 -.x2)**2.) +.((y1 -.y2)**2.)))
+	;;
+
+(*
+let () =distanza ~-. 3.05 5.1 2.85 ~-. 8.6;;
+*)
+let () =
+distanza (float_of_string Sys.argv.(1))( float_of_string Sys.argv.(2) )(float_of_string Sys.argv.(3))(float_of_string Sys.argv.(4))
+
+
+
+
+
+
+
