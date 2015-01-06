168408f97456fdd9021dfa6e7f2adabb99849ea1
danielefrisanco <daniele.frisanco@gmail.com>
2015-01-06 16:22:26 +0100

    t-sql quadratic equation function changed to return a table

--- tsql/quadratic_equation.sql
+++ tsql/quadratic_equation.sql
@@ -1,42 +1,57 @@
 
-
+/*
 IF OBJECT_ID (N'dbo.quadratic_equation', N'FN') IS NOT NULL
 	DROP FUNCTION quadratic_equation;
-GO
+GO*/
 CREATE FUNCTION dbo.quadratic_equation (@a FLOAT
 										,@b FLOAT
 										,@c float  )
-RETURNS varchar (100)
+RETURNS   @ret TABLE
+   (
+    x1real     FLOAT,
+    x1imm       FLOAT,
+    x2real   FLOAT,
+    x2imm   FLOAT
+   )
+AS
 AS  
-BEGIN
-
-	DECLARE @ret  varchar(100);
+BEGIN 
 	declare @delta float ;
 	declare @denom float ;
+	DECLARE @nat FLOAT;
+				DECLARE @imm FLOAT;
 	select @delta=(POWER(@b,2)-4*@a*@c);
 	select @denom=(2*@a);
 	if (@delta>=0)    
 		BEGIN
-			DECLARE @x12 FLOAT;
-			SELECT @x12=((-@b+SQRT(@delta))/@denom);
-			SELECT @ret='@x1='+ CONVERT(varchar(100),@x12 )  
-						+' @x2='+ CONVERT(varchar(100),@x12 ) 
+			 
+			SELECT 	@x12=((-@b+SQRT(@delta))/@denom);
+			insert @ret
+			SELECT 	CONVERT(varchar(100),@x12 )  
+					,0
+					,-CONVERT(varchar(100),@x12 ) 
+					,0
+			 
 		END 
 		ELSE 
 			BEGIN  
-				DECLARE @nat FLOAT;
-				DECLARE @imm FLOAT;
 				 
-				SELECT @nat=((-@b)/@denom);
-				SELECT @imm=((SQRT(-@delta))/@denom);
-				SELECT @ret='@x1='+ CONVERT(varchar(100),@nat)+'+i'+CONVERT(varchar(100),@imm ) 
-							+' @x2='+ CONVERT(varchar(100),@nat)+'-i'+CONVERT(varchar(100),@imm )
+				 
+				SELECT 	@nat=((-@b)/@denom);
+				SELECT 	@imm=((SQRT(-@delta))/@denom);
+				insert @ret
+				SELECT 	CONVERT(varchar(100),@nat)
+						,CONVERT(varchar(100),@imm ) 			
+						,CONVERT(varchar(100),@nat)
+						,-CONVERT(varchar(100),@imm )
+				 
 			end
-	RETURN @ret ;
+	RETURN  
 
-END;
-GO
+END
+/*;
+GO*/
+ 
 
-
-SELECT dbo.quadratic_equation(1,2,1)  
-SELECT dbo.quadratic_equation(1,2,3)  
+SELECT * FROM dbo.quadratic_equation(1,2,1)  
+SELECT * FROM  dbo.quadratic_equation(1,2,3)  
